06d816a7ecb39901fc7a0079e4b8e13664ac83c2
baecheolhyeon <todd1130@naver.com>
2025-06-10 20:27:34 +0900

    [start] init commit

---
+++ src/main/java/com/oreum/zzarit/ZzaritApplication.java
@@ -0,0 +1,13 @@
+package com.oreum.zzarit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzaritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzaritApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oreum/zzarit/ZzaritApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oreum.zzarit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzaritApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
