85acb49930d86692f478cad84875fef3eb19bc53
b1anker-kr <b1anker.kr@gmail.com>
2025-06-19 20:31:44 +0900

    create .github folder

--- src/main/java/com/oreum/zzarit/global/config/SwaggerConfig.java
+++ src/main/java/com/oreum/zzarit/global/config/SwaggerConfig.java
@@ -1,23 +1,23 @@
-package com.oreum.zzarit.global.config;
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-
-@Configuration
-public class SwaggerConfig {
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .components(
-                        new Components()
-                                .addSecuritySchemes("accessToken", new SecurityScheme()
-                                        .name("accessToken")
-                                        .type(SecurityScheme.Type.APIKEY)
-                                        .in(SecurityScheme.In.HEADER)
-                                        .bearerFormat("JWT")
-                                )
-                );
-    }
-}
+//package com.oreum.zzarit.global.config;
+//
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//
+//@Configuration
+//public class SwaggerConfig {
+//    @Bean
+//    public OpenAPI customOpenAPI() {
+//        return new OpenAPI()
+//                .components(
+//                        new Components()
+//                                .addSecuritySchemes("accessToken", new SecurityScheme()
+//                                        .name("accessToken")
+//                                        .type(SecurityScheme.Type.APIKEY)
+//                                        .in(SecurityScheme.In.HEADER)
+//                                        .bearerFormat("JWT")
+//                                )
+//                );
+//    }
+//}
