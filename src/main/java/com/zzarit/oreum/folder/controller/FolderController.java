d7b73b049cce290468ce6296ab75fe7bbd1ff1e1
b1anker-kr <b1anker.kr@gmail.com>
2025-06-19 23:50:11 +0900

    [feat] 폴더 생성 Api 구현

--- src/main/java/com/zzarit/oreum/folder/controller/FolderController.java
+++ src/main/java/com/zzarit/oreum/folder/controller/FolderController.java
@@ -1,6 +1,16 @@
 package com.zzarit.oreum.folder.controller;
 
+import com.zzarit.oreum.folder.domain.Folder;
+import com.zzarit.oreum.folder.service.FolderService;
+import com.zzarit.oreum.folder.service.dto.FolderRequestDto;
+import com.zzarit.oreum.folder.service.dto.FolderResponseDto;
+import com.zzarit.oreum.member.domain.Member;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/folder")
 public class FolderController {
+
+    private final FolderService folderService;
+
+    @PostMapping("/create-folder")
+    public ResponseEntity<Void> createFolder(@Parameter(hidden = true) Member member, @RequestBody FolderRequestDto request) {
+        folderService.createFolder(request, member);
+
+        return ResponseEntity.noContent().build();
+    };
 }
---
+++ src/main/java/com/zzarit/oreum/folder/service/dto/FolderRequestDto.java
@@ -0,0 +1,4 @@
+package com.zzarit.oreum.folder.service.dto;
+
+public record FolderRequestDto(String name) {
+}
