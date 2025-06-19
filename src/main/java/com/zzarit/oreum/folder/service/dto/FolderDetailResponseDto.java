9fcc79cc2797287394627f2e06dfe28c080f9236
b1anker-kr <b1anker.kr@gmail.com>
2025-06-20 05:27:03 +0900

    [feat] 폴더 장소 불러오기 Api 구현

--- src/main/java/com/zzarit/oreum/folder/service/dto/FolderDetailResponseDto.java
+++
@@ -1,7 +0,0 @@
-package com.zzarit.oreum.folder.service.dto;
-
-import com.zzarit.oreum.folder.domain.Folder;
-import com.zzarit.oreum.place.domain.Place;
-
-public record FolderDetailResponseDto(Long folderPlaceId, Folder folder, Place place) {
-}
---
+++ src/main/java/com/zzarit/oreum/folder/service/dto/FolderPlaceResponseDto.java
@@ -0,0 +1,10 @@
+package com.zzarit.oreum.folder.service.dto;
+
+import com.zzarit.oreum.folder.domain.FolderPlace;
+
+public record FolderPlaceResponseDto(Long folderPlaceId, Long FolderId, Long placeId, String placeTitle,
+                                     String placeAddress, String placeThumbnailImage) {
+    public FolderPlaceResponseDto(FolderPlace folderPlace) {
+        this(folderPlace.getId(), folderPlace.getFolder().getId(), folderPlace.getPlace().getId(), folderPlace.getPlace().getTitle(), folderPlace.getPlace().getAddress(), folderPlace.getPlace().getThumbnailImage());
+    }
+}
