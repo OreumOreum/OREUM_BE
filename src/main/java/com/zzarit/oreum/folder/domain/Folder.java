784727ca83746efe07f10bf5279d3cb1e7d98f95
baecheolhyeon <todd1130@naver.com>
2025-07-23 01:22:06 +0900

    [fix] 기본폴더 컬럼 추가

--- src/main/java/com/zzarit/oreum/folder/domain/Folder.java
+++ src/main/java/com/zzarit/oreum/folder/domain/Folder.java
@@ -3,6 +3,7 @@ package com.zzarit.oreum.folder.domain;
 import com.zzarit.oreum.global.domain.BaseTimeEntity;
 import com.zzarit.oreum.member.domain.Member;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-@Setter
 @Entity
 @Table(name = "folder")
 @NoArgsConstructor
@@ -21,18 +21,25 @@ public class Folder extends BaseTimeEntity {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @Setter
     @Column(name = "name")
     private String name;
 
+    @Setter
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
+    @Setter(AccessLevel.NONE)
+    @Column(name = "is_default", updatable = false)
+    private boolean isDefault;
+
     @OneToMany(mappedBy = "folder", cascade = CascadeType.REMOVE, orphanRemoval = true)
     private List<FolderPlace> folderPlaces = new ArrayList<>();
 
-    public Folder(String name, Member member) {
+    public Folder(String name, Member member, boolean isDefault) {
         this.name = name;
         this.member = member;
+        this.isDefault = isDefault;
     }
 }
