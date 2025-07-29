e7d1e9214557910f081e898eafda89be1b7c3678
baecheolhyeon <todd1130@naver.com>
2025-07-30 01:10:33 +0900

    [feat] 카테고리 매핑 초기화

---
+++ src/main/java/com/zzarit/oreum/place/domain/repository/CategoryMapRepository.java
@@ -0,0 +1,4 @@
+package com.zzarit.oreum.place.domain.repository;
+
+public interface CategoryMapRepository extends org.springframework.data.jpa.repository.JpaRepository<com.zzarit.oreum.place.domain.CategoryMap, com.zzarit.oreum.member.domain.SubCategory> {
+  }
