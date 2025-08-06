39cc74a98750afdd868a14ddf83cd8874740a9d4
baecheolhyeon <todd1130@naver.com>
2025-08-07 02:50:40 +0900

    [fix] planner 상세보기 API: 좌표, 관광타입 추가

--- src/main/java/com/zzarit/oreum/planner/service/dto/PlannerPlaceResponseDto.java
+++ src/main/java/com/zzarit/oreum/planner/service/dto/PlannerPlaceResponseDto.java
@@ -4,8 +4,14 @@ import com.zzarit.oreum.planner.domain.PlannerPlace;
 
 public record PlannerPlaceResponseDto(Long plannerPlaceId, int day, int order, Long plannerId, Long placeId,
                                       String placeTitle,
-                                      String placeAddress, String placeThumbnailImage) {
+                                      String placeAddress,
+                                      String placeThumbnailImage,
+                                      String contentTypeId,
+                                      double mapX,
+                                      double mapY) {
     public PlannerPlaceResponseDto(PlannerPlace plannerPlace) {
-        this(plannerPlace.getId(), plannerPlace.getSequenceDay(), plannerPlace.getSequenceOrder(), plannerPlace.getPlanner().getId(), plannerPlace.getPlace().getId(), plannerPlace.getPlace().getTitle(), plannerPlace.getPlace().getAddress(), plannerPlace.getPlace().getThumbnailImage());
+        this(plannerPlace.getId(), plannerPlace.getSequenceDay(), plannerPlace.getSequenceOrder(), plannerPlace.getPlanner().getId(), plannerPlace.getPlace().getId(), plannerPlace.getPlace().getTitle(), plannerPlace.getPlace().getAddress(), plannerPlace.getPlace().getThumbnailImage()
+                ,plannerPlace.getPlace().getContentTypeId(),plannerPlace.getPlace().getMapx(),plannerPlace.getPlace().getMapy()
+        );
     }
 }
