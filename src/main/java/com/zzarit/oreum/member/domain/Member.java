e5d9a259e7279f6471288c83c5e47e9a257eff8f
baecheolhyeon <todd1130@naver.com>
2025-08-11 01:03:55 +0900

    [fix] badge 엔티티 삭제

--- src/main/java/com/zzarit/oreum/member/domain/Member.java
+++ src/main/java/com/zzarit/oreum/member/domain/Member.java
@@ -4,7 +4,6 @@ import com.zzarit.oreum.folder.domain.Folder;
 import com.zzarit.oreum.global.domain.BaseTimeEntity;
 import com.zzarit.oreum.place.domain.Review;
 import com.zzarit.oreum.planner.domain.Planner;
-import com.zzarit.oreum.spot.domain.Badge;
 import com.zzarit.oreum.spot.domain.VisitLog;
 import jakarta.persistence.*;
 import lombok.Builder;
@@ -36,10 +35,6 @@ public class Member extends BaseTimeEntity {
     @Column(name = "email")
     private String email;
 
-    @ColumnDefault("0") // DB 레벨에서 기본값을 0으로 설정
-    @Column(name = "badge_count", nullable = false)
-    private Integer badgeCount;
-
     @Column(name = "fcm_token")
     private String fcmToken;
 
@@ -56,8 +51,6 @@ public class Member extends BaseTimeEntity {
     @OneToMany(mappedBy = "member", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
     private List<Folder> folders = new ArrayList<>();
 
-    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
-    private List<Badge> badges = new ArrayList<>();
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
     private List<Planner> planners = new ArrayList<>();
@@ -78,7 +71,6 @@ public class Member extends BaseTimeEntity {
     public Member(String loginId){
         this.loginId =loginId;
         this.addFolder(new Folder("모든 저장됨", this, true));
-        this.badgeCount = 0;
     }
 
 }
