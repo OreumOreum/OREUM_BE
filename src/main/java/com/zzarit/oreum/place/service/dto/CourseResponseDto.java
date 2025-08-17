2257bce71eef3306145c8e67f012501ab08a5d69
baecheolhyeon <todd1130@naver.com>
2025-08-17 17:39:07 +0900

    [feat] 코스 리스트에 contentID 추가

--- src/main/java/com/zzarit/oreum/place/service/dto/CourseResponseDto.java
+++ src/main/java/com/zzarit/oreum/place/service/dto/CourseResponseDto.java
@@ -4,6 +4,8 @@ import com.zzarit.oreum.place.domain.Course;
 
 public record CourseResponseDto(
         Long id,
+        String contentId,
+        String contentTypeId,
         String title,
         String originImage,
         String thumbnailImage,
@@ -14,6 +16,8 @@ public record CourseResponseDto(
     public static CourseResponseDto from(Course course) {
         return new CourseResponseDto(
                 course.getId(),
+                course.getContentId(),
+                "25",
                 course.getTitle(),
                 course.getOriginImage(),
                 course.getThumbnailImage(),
