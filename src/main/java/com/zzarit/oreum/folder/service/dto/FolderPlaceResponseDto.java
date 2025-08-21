39d3f5b7051df034ae76fe75f93dccc695a1102a
baecheolhyeon <todd1130@naver.com>
2025-08-21 18:46:03 +0900

    [fix] dto 수정: contentID 추가

--- src/main/java/com/zzarit/oreum/folder/service/dto/FolderPlaceResponseDto.java
+++ src/main/java/com/zzarit/oreum/folder/service/dto/FolderPlaceResponseDto.java
@@ -2,10 +2,23 @@ package com.zzarit.oreum.folder.service.dto;
 
 import com.zzarit.oreum.folder.domain.FolderPlace;
 
-public record FolderPlaceResponseDto(Long folderPlaceId, Long FolderId, Long placeId, String placeTitle,
-                                     String placeAddress, String originImage, String contentTypeId) {
+public record FolderPlaceResponseDto(Long folderPlaceId,
+                                     Long FolderId,
+                                     Long placeId,
+                                     String placeTitle,
+                                     String placeAddress,
+                                     String originImage,
+                                     String contentId,
+                                     String contentTypeId) {
     public FolderPlaceResponseDto(FolderPlace folderPlace) {
-        this(folderPlace.getId(),folderPlace.getFolder().getId(), folderPlace.getPlace().getId(), folderPlace.getPlace().getTitle(), folderPlace.getPlace().getAddress(), folderPlace.getPlace().getOriginImage(),
-                folderPlace.getPlace().getContentTypeId());
+        this(folderPlace.getId(),
+                folderPlace.getFolder().getId(),
+                folderPlace.getPlace().getId(),
+                folderPlace.getPlace().getTitle(),
+                folderPlace.getPlace().getAddress(),
+                folderPlace.getPlace().getOriginImage(),
+                folderPlace.getPlace().getContentId(),
+                folderPlace.getPlace().getContentTypeId()
+        );
     }
 }
